7df7fd9b58fd62b686ab93f59c4ace2ea1312e73
Carl de Billy <carl.debilly@gmail.com>
2020-06-09 09:17:47 -0400

    feat(msal): Added new Uno.UI.MSAL add-in project file into solution.

---
+++ src/AddIns/Uno.UI.MSAL/AbstractApplicationBuilderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Identity.Client;
+
+namespace Uno.UI.MSAL
+{
+	public static class AbstractApplicationBuilderExtensions
+	{
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T WithUnoHelpers<T>(this T builder)
+			where T : AbstractApplicationBuilder<T>
+		{
+#if __ANDROID__
+				.WithParentActivityOrWindow(() => Uno.UI.ContextHelper.Current as Android.App.Activity)
+#elif __IOS__ || __MACOS__
+				.WithParentActivityOrWindow(() => Window.RootViewController)
+#elif __WASM__
+			builder.WithHttpClientFactory(WasmHttpFactory.Instance);
+#endif
+			return builder;
+
+		}
+	}
+}
